02f5841e879be81d65e49ad5d02516823060c9dc
Joe Gesualdo <joegesualdo@gmail.com>
2016-07-26 08:45:56 -0400

    Fix

--- examples/example.js
+++ examples/example.js
@@ -1,7 +1,105 @@
 const vttToJson = require("./../index.js")
 
-const fileName = __dirname + '/meow.en.vtt';
+// const fileName = __dirname + '/meow.en.vtt';
+let vttString =`WEBVTT
+Kind: captions
+Language: en
+Style:
+::cue(c.colorCCCCCC) { color: rgb(204,204,204);
+ }
+::cue(c.colorE5E5E5) { color: rgb(229,229,229);
+ }
+##
 
-vttToJson(fileName, function(err, obj){
-  console.log(obj)
+00:00:00.000 --> 00:00:06.600 align:start position:0%
+
+here<00:00:04.980><c> we</c><00:00:05.190><c> have</c><c.colorCCCCCC><00:00:05.400><c> Alex</c></c><00:00:06.060><c> on</c><00:00:06.270><c> media</c><c.colorE5E5E5><00:00:06.600><c> row</c></c>
+
+00:00:06.600 --> 00:00:06.899 align:start position:0%
+here we have<c.colorCCCCCC> Alex</c> on media<c.colorE5E5E5> row
+ </c>
+
+00:00:06.899 --> 00:00:09.000 align:start position:0%
+here we have<c.colorCCCCCC> Alex</c> on media<c.colorE5E5E5> row</c>
+at<00:00:07.109><c> the</c><c.colorE5E5E5><00:00:07.170><c> RNC</c></c><c.colorCCCCCC><00:00:07.589><c> Alex</c></c><00:00:08.490><c> reaches</c><00:00:09.000><c> the</c>
+
+00:00:09.000 --> 00:00:09.120 align:start position:0%
+at the<c.colorE5E5E5> RNC</c><c.colorCCCCCC> Alex</c> reaches the
+
+
+00:00:09.120 --> 00:00:10.980 align:start position:0%
+at the<c.colorE5E5E5> RNC</c><c.colorCCCCCC> Alex</c> reaches the
+<c.colorCCCCCC>young<00:00:09.389><c> turks</c><00:00:09.630><c> you</c><00:00:10.050><c> to</c><00:00:10.260><c> booth</c></c><c.colorE5E5E5><00:00:10.679><c> and</c></c><00:00:10.980><c> is</c>
+
+00:00:10.980 --> 00:00:11.070 align:start position:0%
+<c.colorCCCCCC>young turks you to booth</c><c.colorE5E5E5> and</c> is
+
+
+00:00:11.070 --> 00:00:12.570 align:start position:0%
+<c.colorCCCCCC>young turks you to booth</c><c.colorE5E5E5> and</c> is
+instantly<00:00:11.580><c> met</c><00:00:11.910><c> by</c><00:00:12.150><c> a</c><00:00:12.179><c> group</c><00:00:12.570><c> of</c>`
+
+// let vttString = `WEBVTT
+// Kind: captions
+// Language: en
+//
+// 00:00:00.867 --> 00:00:02.936
+// Mr. Earnest: Good
+// afternoon, everybody.
+//
+// 00:00:02.936 --> 00:00:05.405
+// It's nice to
+// see all of you.
+//
+// 00:00:05.405 --> 00:00:07.006
+// For those of you who are
+// paying close attention
+//
+// 00:00:07.006 --> 00:00:08.341
+// to my personal schedule
+// today, you know that
+//
+// 00:00:08.341 --> 00:00:11.044
+// I went and appeared at
+// a forum with the Center
+//
+// 00:00:11.044 --> 00:00:14.914
+// of American Progress to
+// talk about men and fathers
+//
+// 00:00:14.914 --> 00:00:17.484
+// and work-life
+// family balance.
+//
+// 00:00:17.484 --> 00:00:19.486
+// I'm much less nervous
+// sitting here talking
+//
+// 00:00:19.486 --> 00:00:21.488
+// to you about a range of
+// foreign policy issues,
+//
+// 00:00:21.488 --> 00:00:23.623
+// and even some domestic
+// ones, than I am talking
+//
+// 00:00:23.623 --> 00:00:25.792
+// about my personal
+// life with my wife
+//
+// 00:00:25.792 --> 00:00:27.293
+// and child in
+// the front row.
+//
+// 00:00:27.293 --> 00:00:29.629
+// So for one day I can at
+// least walk in here and
+//
+// 00:00:29.629 --> 00:00:30.964
+// say I'm relieved to
+// see all of you.`
+//
+vttToJson(vttString)
+.then(sections => {
+  console.log(sections)
 });
--- index.js
+++ index.js
@@ -3,52 +3,159 @@ const fs = require('fs');
 
 const fileName = 'meow.en.vtt';
 
-function convertSectionArrayToObject(sectionArray){
-  return {
-    id: sectionArray[0],
-    payload: sectionArray.slice(1)
-  }
-}
 
-function convertAllSectionArraysToObject(sectionsArray) {
-  items = []
-  sectionsArray.forEach(function(item, index){
-    if (index === 0) {
-      // This doesn't add the first part (ie. WEBVTT...)
-      return
+// function convertSectionArrayToObject(sectionArray){
+//   return {
+//     id: sectionArray[0],
+//     payload: sectionArray.slice(1)
+//   }
+// }
+//
+// function convertAllSectionArraysToObject(sectionsArray) {
+//   items = []
+//   sectionsArray.forEach(function(item, index){
+//     if (index === 0) {
+//       // This doesn't add the first part (ie. WEBVTT...)
+//       return
+//     } else {
+//       items.push(convertSectionArrayToObject(item))
+//     }
+//   })
+//   return {
+//     items: items
+//   }
+// }
+//
+// function convertVttToJson(vttString, callback) {
+//   var currentSectionArray = []
+//   var allSectionsArray = []
+//
+//  vttString.split('\n').forEach((line) => {
+//     if (line == " "){
+//       currentSectionArray.push(line)
+//     } else if (line == ""){
+//       allSectionsArray.push(currentSectionArray)
+//       currentSectionArray = []
+//     } else {
+//       currentSectionArray.push(line)
+//     }
+//   })
+//   // Since when we close the file, we still have the last section in the current variable, we add and empty it here
+//   allSectionsArray.push(currentSectionArray)
+//   currentSectionArray = []
+//
+//   callback(null, convertAllSectionArraysToObject(allSectionsArray))
+// }
+
+
+
+
+function convertVttToJson(vttString) {
+  return new Promise((resolve, reject) => {
+  var current = {}
+  var sections = []
+  var start = false;
+   vttString.split('\n').forEach((line) => {
+    // console.log(line)
+    if (line === " "){
+      // current.push(line)
+    } else if (line == "") {
+    } else if (line.indexOf('-->') !== -1) {
+      start = true;
+      if (current.start) {
+        sections.push(current)
+      }
+      // console.log(line.split("-->"))
+      current = {
+        start: line.split("-->")[0].trimRight().split(" ").pop(),
+        end: line.split("-->")[1].trimLeft().split(" ").shift(),
+        part: ''
+      }
+    // } else if (line == ""){
+      // not auto
+      // current.shift();
+      // sections = sections.concat(current)
+      // for auto
+      // console.log(current)
+      // sections.push(current[current.length - 1])
+      // current = []
+    } else if (line.replace(/<\/?[^>]+(>|$)/g, "") === ""){
+    } else if (line.replace(/<\/?[^>]+(>|$)/g, "") === " "){
     } else {
-      items.push(convertSectionArrayToObject(item))
+      if (start){
+        if (sections.length === 0) {
+          sections.push(current)
+        } else {
+          if (sections[sections.length - 1].part.replace(/<\/?[^>]+(>|$)/g, "") === line.replace(/<\/?[^>]+(>|$)/g, "")) {
+            // duplicate: don't do anything
+            // console.log('dups')
+          } else {
+            current.part = line
+            // sections.push(lne)
+          }
+        }
+      }
+      // console.log(line.replace(/<\/?[^>]+(>|$)/g, ""));
+      // cleanText = results[0].replace(/<\/?[^>]+(>|$)/g, "");
     }
   })
-  return {
-    items: items
-  }
-}
+  // console.log(sections)
+    // console.log(current)
+  // console.log(sections)
+  // Since when we close the file, we still have the last section in the current variable, we add and empty it here
+  // sections.push(current[current.length - 1])
+  current = []
 
-function convertVttToJson(fileName, callback) {
-  var currentSectionArray = []
-  var allSectionsArray = []
-
-  var lineReader = readLine.createInterface({
-    input: fs.createReadStream(fileName)
-  });
-
-  lineReader.on('line', function (line) {
-    if (line == " "){
-      currentSectionArray.push(line)
-    } else if (line == ""){
-      allSectionsArray.push(currentSectionArray)
-      currentSectionArray = []
-    } else {
-      currentSectionArray.push(line)
-    }
+  var regex = /(<([0-9:.>]+)>)/ig
+  // console.log(sections)
+  sections.forEach(section => {
+    // console.log(sections)
+    // strs = section.part.join(" ").split()
+    strs = section.part.split()
+    // console.log(strs)
+    // console.log(strs[strs.length - 5])
+    var results = strs.map(function(s){
+        return s.replace(regex, function(n){
+          return n.split('').reduce(function(s,i){ return `==${n.replace("<", "").replace(">", "")}` }, 0)
+        })
+    });
+    // console.log(results)
+    cleanText = results[0].replace(/<\/?[^>]+(>|$)/g, "");
+    cleanArray = cleanText.split(" ")
+    // resultsObj = {}
+    resultsArray = [];
+    // console.log(cleanArray)
+    // console.log(cleanArray)
+    cleanArray.forEach(function(item){
+      if (item.indexOf('==') > -1) {
+        var pair = item.split("==")
+        var key = pair[0]
+        var value = pair[1]
+        if(key == "" || key == "##") {
+          return;
+        }
+        // console.log(item)
+        resultsArray.push({
+          word: item.split("==")[0],
+          time: item.split("==")[1],
+        })
+        // resultsObj[item.split("==")[0]] = item.split("==")[1]
+      } else {
+        // console.log(item)
+        resultsArray.push({
+          word: item,
+          time: undefined,
+        })
+        // resultsObj[item] = undefined
+        // console.log(resultsObj)
+      }
+    })
+    section.words = resultsArray;
   })
-  lineReader.on('close', function (line) {
-    // Since when we close the file, we still have the last section in the current variable, we add and empty it here
-    allSectionsArray.push(currentSectionArray)
-    currentSectionArray = []
-
-    callback(null, convertAllSectionArraysToObject(allSectionsArray))
+  // console.log(sections[sections.length - 2])
+  // console.log(sections[sections.length - 3])
+  // console.log(sections.join(" ").split(regex))
+    resolve(sections);
   })
 }
 
